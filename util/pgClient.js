f6ba803469d4e3eedb255323220f2c54ec8352a3
volaka <hasancanvolaka@feynlab.io>
2019-06-04 23:49:33 +0300

    fix/production server database connection

--- util/pgClient.js
+++ util/pgClient.js
@@ -1,11 +1,9 @@
 import { Client } from 'pg';
 
-const connectionString = process.env.DATABASE_URL;
-
 export const getClient = () => {
   // If production database has connection url then use this endpoint,
   // else you can delete it.
-  if (process.env.DEBUG === undefined) return new Client({ connectionString });
+  // if (process.env.DEBUG === undefined) return new Client({ connectionString });
   return new Client({
     user: process.env.DATABASE_USER,
     host: process.env.DATABASE_HOST,
